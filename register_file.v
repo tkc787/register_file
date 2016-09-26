4bd933aff8f60c0e3010a69847e25dd16e2c48b3
tkc787 <tarik.calderon@upr.edu>
2016-09-26 00:10:16 -0400

    created 16to1 mux and mux tester

--- register_file.v
+++ register_file.v
@@ -3,41 +3,57 @@ module binary_decoder (output reg [15:0] O, input [3:0] D, input Ld);
 	always @(Ld, D) 
 	begin
 		if(Ld) begin
-//			case(D)
-//				'h0:	O = 0;
-//				'h1:	O = 2;
-//				'h2:	O = 4;
-//				'h3:	O = 8;
-//				'h4:	O = 16;
-//				'h5:	O = 32;
-//				'h6:	O = 64;
-//				'h7:	O = 128;
-//				'h8:	O = 256;
-//				'h9:	O = 512;
-//				'ha:	O = 1024;
-//				'hb:	O = 2048;
-//				'hc:	O = 4096;
-//				'hd:	O = 8192;
-//				'he:	O = 16384;
-//				'hf:	O = 32768;
-//			endcase
 			if(D == 0)
-				O = 0;
+				O = 1;
 			else
 				O = result << D;
+		end
+		else begin
+			O = 0;
 		end
 	end
 endmodule
 
-module decoder_test;
-	reg Ld; wire [15:0] O; reg [3:0] D;
-	binary_decoder test(O, D, Ld);
+module Mux_16_1 (output reg [31:0] Y, input [3:0] S, input [31:0] data[0:15]);
+	always @ (S)
+		Y = data[S];
+endmodule
+
+module Register (output reg [31:0]);
+
+endmodule
+
+module Mux_16_test;
+	reg [31:0] R[0:15];
+	wire [31:0] Y;
+	reg [3:0] S;
+	Mux_16_1 mux_test(Y, S, R);
 	initial #100 $finish;
 	initial begin
-		D = 'h0;
-		Ld = 1'b0;
-		Ld = 1'b1;
-		repeat (15) #5 D = D + 'h1;
+		S = 'h0;
+		R[0] = 'hFFFFFFFF;
+		R[1] = 'h0000FFFF;
+		R[2] = 'hFFFF0000;
+		R[3] = 'hAAAA0000;
+		R[4] = 'hAABBAABB;
+		repeat (4) #5 S = S + 'h1;
 	end
-	initial $monitor("Output = %b ", O);	
+	initial $monitor("Output = %b ", Y);
 endmodule
+
+// module decoder_test;
+// 	reg Ld;
+// 	wire [15:0] O;
+// 	reg [3:0] D;
+// 	binary_decoder test(O, D, Ld);
+// 	initial #100 $finish;
+// 	initial begin
+// 		D = 'h0;
+// 		Ld = 1'b0;
+// 		// Ld = 1'b1;
+// 		repeat (15) #5 D = D + 'h1;
+// 	end
+// 	initial $monitor("Output = %b ", O);	
+// endmodule
+
+
